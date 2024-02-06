8dcbb534352162cafc58c6cb1634b749454b5164
jihuayu <jihuayu123@gmail.com>
2024-02-06 11:24:10 +0800

    add x.js

--- script/x.js
+++ script/x.js
@@ -1,48 +1,48 @@
 const fs = require("fs")
-const regex = /ui\.localize\((['"`])([\s\S]*?)\1/gm;
-const regex2 = /l10n\.t\((['"`])([\s\S]*?)\1/gm;
 
-const str = fs.readFileSync("./out/bundle.js")
+if(process.argv.length < 3){
+    console.log(`Usage: node x.js <lang>`)
+    process.exit(1)
+}
 
-const zh = fs.readFileSync("./l10n/bundle.l10n.zh-cn.json")
-const zhl = JSON.parse(zh)
+const lang = process.argv[2]
 
-let m;
-let ret = new Map()
-while ((m = regex.exec(str)) !== null) {
-    // This is necessary to avoid infinite loops with zero-width matches
-    if (m.index === regex.lastIndex) {
-        regex.lastIndex++;
-    }
+const regexUI = /ui\.localize\((['"`])([\s\S]*?)\1/gm;
+const regex2L10n = /l10n\.t\((['"`])([\s\S]*?)\1/gm;
+const codeStr = fs.readFileSync("./out/bundle.js")
 
-    // The result can be accessed through the `m`-variable.
-    m.forEach((match, groupIndex) => {
-        if (groupIndex == 2) {
-            ret[match] = match
-            console.log(`Found match, group ${groupIndex}: ${match}`);
+let lang_file = "{}"
+if (fs.existsSync(`./l10n/bundle.l10n.${lang}.json`)) {
+    lang_file = fs.readFileSync(`./l10n/bundle.l10n.${lang}.json`, { encoding: 'utf8' });
+}
+const oldLangMap = JSON.parse(lang_file.replace("\\", "\\\\"))
+
+let newLangMap = new Map()
+
+function fillMap(regex,map) {
+    while ((m = regex.exec(codeStr)) !== null) {
+        if (m.index === regex.lastIndex) {
+            regex.lastIndex++;
         }
-    });
-}
-
-while ((m = regex2.exec(str)) !== null) {
-    // This is necessary to avoid infinite loops with zero-width matches
-    if (m.index === regex2.lastIndex) {
-        regex2.lastIndex++;
-    }
-
-    // The result can be accessed through the `m`-variable.
-    m.forEach((match, groupIndex) => {
-        if (groupIndex == 2) {
-            ret[match] = match
-            console.log(`Found l10n key: ${match}`);
-        }
-    });
-}
-
-for (const key in ret) {
-    if (zhl[key]) {
-        ret[key] = zhl[key]
+    
+        m.forEach((match, groupIndex) => {
+            if (groupIndex == 2) {
+                map[match] = match
+            }
+        });
     }
 }
 
-fs.writeFileSync("./1.json", JSON.stringify(ret, null, 2))
+// found all l10n key use `ui.localize`
+fillMap(regexUI,newLangMap)
+// found all l10n key use `t.l10n`
+fillMap(regex2L10n,newLangMap)
+
+// merge with existing l10n file
+for (const key in newLangMap) {
+    if (oldLangMap[key]) newLangMap[key] = oldLangMap[key]
+}
+
+fs.writeFileSync(`./l10n/bundle.l10n.${lang}.json`, JSON.stringify(newLangMap, null, 2).replace("\\\\", "\\"), { encoding: 'utf8' })
+
+console.log(`Updated l10n file for lang: ${lang}`)
